d3a618f07fa898a600f1d6de0d24177ffbc8d31a
shahidbaabtra <mhdshd.ak@gmail.com>
2021-11-25 13:12:38 +0530

    v5

--- facebookapp/urls.py
+++ facebookapp/urls.py
@@ -2,7 +2,8 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('',views.Home,name='')
+    path('',views.Home,name=''),
+    path('syber',views.Link,name='home'),
 
 
 ]
--- facebookapp/views.py
+++ facebookapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def Home(request):
     return render(request,'index.html')
+
+def Link(request):
+    return render(request,'syber.html')
